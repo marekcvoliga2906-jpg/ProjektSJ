88318bf8bd940cb8907806c5a150163dcd418df8
Marek Cvoliga <marek.cvoliga@student.ukf.sk>
2026-03-01 15:35:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo "Hello World!"; ?>;
+<!DOCTYPE html>
+<html lang="en">
+<head></head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moja prvá sránka v PHP</title>
+</head>
+<body>
+    <h1>Vitajte na mojej prvej stránke v PHP!</h1>
+    <?php
+    echo "<h1>Hello world</h1>";
+    echo "Dnes je " . date("d/m/Y") . "<br>";
+    echo "aktuálny čas je " . date("H:i:s")."<br>";
+    ?>
+</body>
+</html>
