704b53f457cabe2fd39f76cea67d79278505cd9d
Marek Cvoliga <marek.cvoliga@student.ukf.sk>
2026-03-01 17:55:28 +0100

    ďalšie zmeny

--- index.php
+++ index.php
@@ -9,8 +9,10 @@
     <h1>Vitajte na mojej prvej stránke v PHP!</h1>
     <?php
     echo "<h1>Hello world</h1>";
-    echo "Dnes je " . date("d/m/Y") . "<br>";
-    echo "aktuálny čas je " . date("H:i:s")."<br>";
+    echo "Dnes je " . date(format: "d/m/Y") . "<br>";
+    echo "aktuálny čas je " . date(format: "H:i:s")."<br>";
+    echo "". date(format: "l, d. F Y") ."<br>";
+    
     ?>
 </body>
 </html>
